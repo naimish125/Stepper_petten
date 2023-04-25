027e2106a004ab98c0a2f738ba5db14877526865
Naimish <naimishsakhavala125@gmail.com>
2023-04-25 07:31:24 +0530

    24

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     routes: {
-      '/': (context) => HomeScreen(),
+      '/': (context) => SliderScreen(),
     },
   ));
 }
